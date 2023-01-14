4d79ac6f858b4357ee048cf6ba2a5fedaf03cb36
jieggii <jieggii@pm.me>
2023-01-14 21:18:14 +0200

    Refactor, fixes, some extra checks

--- lookupcfg.go
+++ lookupcfg.go
@@ -25,34 +25,34 @@ type IncorrectTypeField struct {
 // ConfigPopulationResult represents result of config population.
 // Reports about mismatches between values provided by source and provided struct.
 type ConfigPopulationResult struct {
-	MissingFields       []Field              // list of fields that are missing
-	IncorrectTypeFields []IncorrectTypeField // array of fields of incorrect type
+	MissingFields       []Field              // slice of fields that are missing
+	IncorrectTypeFields []IncorrectTypeField // slice of fields of incorrect type
 }
 
-// PopulateConfig fills the `object`'s fields with values read from the `source` using `lookupFunction`.
+// PopulateConfig fills the `v`'s fields with values read from the `source` using `lookupFunction`.
 func PopulateConfig(
 	source string,
 	lookupFunction func(string) (string, bool),
-	object interface{},
+	v any,
 ) *ConfigPopulationResult {
 	result := &ConfigPopulationResult{}
 
-	configType := reflect.Indirect(reflect.ValueOf(object)).Type()
+	configType := reflect.Indirect(reflect.ValueOf(v)).Type()
 
 	for i := 0; i < configType.NumField(); i++ { // iterating over struct fields
 		field := configType.Field(i)
-		err, fieldMeta := internal.ParseFieldTag(field.Tag)
+		fieldProperties, err := internal.ParseFieldTag(field.Tag)
 		if err != nil {
 			panic(fmt.Sprintf("Error parsing %v.%v's tag: %v", configType.Name(), field.Name, err))
 		}
-		if !fieldMeta.Participate {
+		if !fieldProperties.Participate {
 			//skip fields which do not participate
 			continue
 		}
-		fieldValue := reflect.ValueOf(object).Elem().Field(i)
+		fieldValue := reflect.ValueOf(v).Elem().Field(i)
 
-		valueName, ok := fieldMeta.ValueSources[source]
-		if !ok { // if `source` provided as function param is not present in the struct's field tag
+		valueName, found := fieldProperties.ValueSources[source]
+		if !found { // if `source` provided as function param is not present in the struct's field tag
 			panic(
 				fmt.Sprintf(
 					"%v.%v does not have tag \"%v\" (for the source \"%v\"). Use `%v` tag if you want to ignore this field.",
@@ -64,9 +64,9 @@ func PopulateConfig(
 				),
 			)
 		}
-		rawValue, ok := lookupFunction(valueName)
-		if !ok { // if value was not received from the provided source
-			if !fieldMeta.DefaultValueWasSet { // if default value of the field was not indicated
+		rawValue, found := lookupFunction(valueName)
+		if !found { // if value was not received from the provided source
+			if !fieldProperties.DefaultValueWasSet { // if default value of the field was not indicated
 				result.MissingFields = append(result.MissingFields, Field{
 					StructName:        field.Name,
 					SourceName:        valueName,
@@ -75,7 +75,7 @@ func PopulateConfig(
 				})
 				continue
 			}
-			rawValue = fieldMeta.DefaultValue
+			rawValue = fieldProperties.DefaultValue
 		}
 		convertedValue, err := internal.ParseValue(rawValue, field.Type)
 		if err != nil {
