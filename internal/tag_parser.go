6d46faba31ab04e5de54f2dcc02a2971fadc47fe
jieggii <jieggii@pm.me>
2023-03-23 09:09:25 +0200

    Update comment for `Participate` field inside `FieldProperties` struct

--- internal/tag_parser.go
+++ internal/tag_parser.go
@@ -10,7 +10,7 @@ import (
 const IgnoranceTag = "lookupcfg:\"ignore\""
 
 type FieldProperties struct {
-	Participate bool // indicates if this field participates in all stuff that this lib does
+	Participate bool // indicates if struct field participates in all stuff that this lib does
 
 	ValueSources       map[string]string // map of sources of value. E.g {"env": "HOST", "json": "host"}
 	DefaultValue       string            // default value is stored as string because we parse it from string
